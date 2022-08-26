0b0319affc9f20372db672450c055adf7cc2e557
erdidemiroz <erdi.demiroz@gmail.com>
2022-08-26 17:07:44 +0300

    options added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,18 +11,19 @@ namespace SampleMemoryCache.Controllers
 
         public HomeController(IMemoryCache memoryCache)
         {
-
             _memoryCache = memoryCache;
         }
 
         public IActionResult Index()
         {
+            var options = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(10))
+                .SetAbsoluteExpiration(DateTime.Now.AddSeconds(30))
+                .SetPriority(CacheItemPriority.High)
+                .RegisterPostEvictionCallback((key, value, reason, state) => { _memoryCache.Set("callback", $"{key}-{value}-{reason}"); });
+
             if (!_memoryCache.TryGetValue("time", out string timeValue))
-                _memoryCache.Set("time", DateTime.Now.ToString(), new MemoryCacheEntryOptions
-                {
-                    SlidingExpiration = TimeSpan.FromSeconds(10),
-                    AbsoluteExpiration = DateTime.Now.AddSeconds(30)
-                });
+                _memoryCache.Set("time", DateTime.Now.ToString(), options);
 
             return View();
         }
@@ -30,6 +31,7 @@ namespace SampleMemoryCache.Controllers
         public IActionResult ShowTime()
         {
             ViewBag.Time = _memoryCache.Get<string>("time");
+            ViewBag.Callback = _memoryCache.Get<string>("callback");
             return View();
         }
 
